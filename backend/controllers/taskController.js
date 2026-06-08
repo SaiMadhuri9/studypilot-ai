7cad55d54eab6c57a64bb88548a1f6c85f10d319
SaiMadhuri9 <appikondamadhuri@gmail.com>
2026-06-08 18:37:58 +0530

    Add PUT task update API

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,19 +1,19 @@
-const getTasks = (req, res) => {
-  const tasks = [
-    {
-      id: 1,
-      title: "Learn Node.js",
-      completed: false,
-      createdAt: new Date()
-    },
-    {
-      id: 2,
-      title: "Build StudyPilot AI",
-      completed: false,
-      createdAt: new Date()
-    }
-  ];
+const tasks = [
+  {
+    id: 1,
+    title: "Learn Node.js",
+    completed: false,
+    createdAt: new Date()
+  },
+  {
+    id: 2,
+    title: "Build StudyPilot AI",
+    completed: false,
+    createdAt: new Date()
+  }
+];
 
+const getTasks = (req, res) => {
   res.status(200).json({
     success: true,
     count: tasks.length,
@@ -21,4 +21,54 @@ const getTasks = (req, res) => {
   });
 };
 
-module.exports = { getTasks }; 
+const createTask = (req, res) => {
+  const { title } = req.body;
+
+  const newTask = {
+    id: 3,
+    title,
+    completed: false,
+    createdAt: new Date()
+  };
+
+  tasks.push(newTask);
+
+  res.status(201).json({
+    success: true,
+    data: newTask
+  });
+};
+
+const updateTask = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const task = tasks.find(task => task.id === id);
+
+  if (!task) {
+    return res.status(404).json({
+      success: false,
+      message: "Task not found"
+    });
+  }
+
+  const { title, completed } = req.body;
+
+  if (title !== undefined) {
+    task.title = title;
+  }
+
+  if (completed !== undefined) {
+    task.completed = completed;
+  }
+
+  res.json({
+    success: true,
+    data: task
+  });
+};
+
+module.exports = {
+  getTasks,
+  createTask,
+  updateTask
+};
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -3,9 +3,17 @@ const router = express.Router();
 console.log("taskRoutes loaded");
 
 // controller function (we will create next)
-const { getTasks } = require("../controllers/taskController");
+
+const {
+  getTasks,
+  createTask,
+  updateTask
+} = require("../controllers/taskController");
 
 // GET all tasks
 router.get("/", getTasks);
+router.post("/", createTask);
+router.put("/:id", updateTask);
+
 
 module.exports = router;
